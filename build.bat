998f2dea84d6170867cb25d5181950ca516ea00f
IvanTopGaming <zjarc0@mail.ru>
2025-07-05 14:53:53 +0300

    Clear indication of the repository commit, deleted Github Actions

--- build.bat
+++ build.bat
@@ -1,7 +1,9 @@
 docker build -t docker.io/ivantopgaming/mini_graalvm:21 -f Dockerfile.graalvm-21 .
 docker build -t docker.io/ivantopgaming/mini_graalvm:24 -f Dockerfile.graalvm-24 .
+docker build -t docker.io/ivantopgaming/mini_graalvm:25 -f Dockerfile.graalvm-25 .
 docker build -t docker.io/ivantopgaming/mini_graalvm:latest .
 
 docker push docker.io/ivantopgaming/mini_graalvm:21
 docker push docker.io/ivantopgaming/mini_graalvm:24
+docker push docker.io/ivantopgaming/mini_graalvm:25
 docker push docker.io/ivantopgaming/mini_graalvm:latest
